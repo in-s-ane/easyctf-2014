1c940bb18b6a92ff38eb884b9ee67be84afe2adc
Danny Qiu <dqiu55@gmail.com>
2014-11-29 23:51:25 -0500

    Add pixelated

--- Pixelated-90/solution.c
+++ Pixelated-90/solution.c
@@ -3,6 +3,9 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+/* After running this, clean up the QR Code in an image editor + invert colors
+ * Flag: pixelsmatterinQRs */
+
 int main() {
     int image1 = open("1.bmp", O_RDONLY);
     int image2 = open("2.bmp", O_RDONLY);
@@ -12,8 +15,15 @@ int main() {
     unsigned char buf1;
     unsigned char buf2;
     unsigned char outputbuf;
+    int same = 1;
     while (read(image1, &buf1, 1) > 0 && read(image2, &buf2, 1) > 0) {
-        outputbuf = buf1 & buf2;
+        if (buf1 == buf2 && same) { // Hack to not alter the header of the file
+            outputbuf = buf1;
+        }
+        else {
+            same = 0;
+            outputbuf = buf1 ^ buf2; // Runs XOR on the individual pixels
+        }
         printf("%d\t%d\t%d\n", buf1, buf2, outputbuf);
         write(output, &outputbuf, 1);
     }
