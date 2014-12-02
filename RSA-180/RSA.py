93ec649945dad8d22212a1cf0b6b17f2cec1ab15
elc1798 <subliminalmau5@gmail.com>
2014-12-02 07:58:52 -0500

    Found something relatively interesting

--- RSA-180/RSA.py
+++ RSA-180/RSA.py
@@ -46,8 +46,12 @@ cipher = int("ac470f7350ea67d7a0696" , 16)
 
 #et cetera.
 
+#Because RSA encryption is a deterministic encryption algorithm (i.e., has no random component) an attacker can successfully launch a chosen plaintext attack against the cryptosystem, by encrypting likely plaintexts under the public key and test if they are equal to the ciphertext. A cryptosystem is called semantically secure if an attacker cannot distinguish two encryptions from each other even if the attacker knows (or has chosen) the corresponding plaintexts. As described above, RSA without padding is not semantically secure.
+
+#Above taken from wikipedia article. From hint: the message includes "RSA" in it
+
 f = open("RSA.out" , 'w')
-possibleFactors = [13 , 59389477894550967316505041]
+possibleFactors = [23 , 14274707 , 3341704042627004137]
 fstreamOutput = ""
 for L in range(0 , len(possibleFactors)):
 	for subset in combinations(possibleFactors , L):
