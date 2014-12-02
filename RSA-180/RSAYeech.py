464bb4b0a53363f62fc938c8bfa19be01f5e879a
Yicheng Wang <alex.wyc2098@protonmail.ch>
2014-12-02 11:40:01 -0500

    updated

--- RSA-180/RSAYeech.py
+++ RSA-180/RSAYeech.py
@@ -7,11 +7,16 @@ c = 13016925875285007045101206L;
 # Assume ethan was right, in that cipher is indeed e, that makes k the mod
 # So d is the modular inverse of c over k
 
+def gcd(a,b): # Euclidean Algorithm
+	while a:
+		a, b = b%a, a;
+	return b;
+
 def egcd(a,b): # Extended Euclidean Algorithm
 	if a == 0:
 		return (b,0,1);
 	else:
-		g,y,x = egcd(b%a,a)
+		g,y,x = egcd(b%a,a);
 		return (g, x - (b // a) * y, y);
 
 def modinv(a,m): # Modular Inverse Finder
@@ -21,11 +26,21 @@ def modinv(a,m): # Modular Inverse Finder
 	else:
 		return x % m;
 
-d = modinv(c,totient); # Doesn't make sense b/c c is not coprime to totient
-hexD = hex(d);
-print "Hex: ";
-print hexD;
+#d = modinv(c,totient); # Doesn't make sense b/c c is not coprime to totient
+#d = modinv(c/2,totient); # Makes 0 sense
 
-Str = hexD[2:]
-print Str;
+for e in range(1000,2000):
+	if gcd(e,totient) == 1:
+		d = modinv(e,totient);
+		m = pow(c,d,k);
+		hexM = hex(m);
+		#print "e: %d" %(e);
+		#print "d: %d" %(d);
+		#print "Hex: %s" %(hex(d));
+		if len(str(hexM)[2:-1]) % 2 == 0:
+			print str(e) + ": " + str(hexM)[2:-1].decode("hex");
+		#print "Result: %s" %(hex(d)[2:].decode("hex"));
 
+
+#Str = hexD[2:]
+#print Str.decode("hex");
