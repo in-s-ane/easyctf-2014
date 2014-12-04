0683b94967a314629893e5bab24456175f6d9ca3
Danny Qiu <dqiu55@gmail.com>
2014-12-04 01:28:39 -0500

    Add Evil Guess! YAY! :D

--- Evil Guess-180/guess.c
+++ Evil Guess-180/guess.c
@@ -1,29 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main()
 {
-	/*printf("Welcome to the guessing game!\n");
-	printf("Please enter your name: ");
-	char * str=(char *)malloc(25);
-	scanf("%20s",str);*/
-        char *str = "Danny";
-	printf("\nHello, %s\nPlease enter in a float to guess: ",str);
-        float * bufA=(float *)malloc(12);
-	float * guess=(float *)bufA;
-	float * answer=(float *)(bufA+4);
-        float num = rand()%1000+1;
-        float den = rand()%1005+1;
-	*answer=num/den;
-	scanf("%lf",guess);
-        printf("\n%f\n%f", num, den);
-	printf("\na: %lf, b: %lf\n",*answer, *guess);
-	if(*answer/(*guess)==1.0)
-	{
-		printf("\nThe flag is '<flag hidden>'\n");
-	}else{
-		printf("\nSorry, that is not the secret number\n");
-	}
-	free(bufA);
-	//free(str);
+        int bufA=(int)malloc(12); // Casts pointer to memory as an int
+        //printf("Pointer to bufA: %d \t Pointer dereferenced: %d\n", bufA, *((int *) bufA));
+        float * guess=(float *)bufA;
+        float * answer=(float *)(bufA+4);
+        //printf("Pointer to guess: %d \t Pointer dereferenced: %d\n", guess, *guess);
+        //printf("Pointer to answer: %d \t Pointer dereferenced: %d\n", answer, *answer);
+        *answer=(rand()%1000+1)/(float)(rand()%1005+1);
+        printf("Answer: %.20f\n", *answer); // Need lotsa precision to get this to work
+        printf("\nPlease enter in a float to guess: ");
+        scanf("%lf",guess);
+        printf("\nanswer: %.10f, guess: %.10f\n",*answer, *guess); // Print out answer and guess with lotsa precision
+        if(*answer/(*guess)==1.0)
+        {
+                printf("\nThe flag is '<flag hidden>'\n");
+        }else{
+                printf("\nSorry, that is not the secret number\n");
+        }
+
+        printf("%d\n", bufA);
+        free(bufA);
 }
