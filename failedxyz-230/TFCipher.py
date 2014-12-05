582aa5a34cbc51d69a61faec81ff305f528c926d
Yicheng Wang <alex.wyc2098@protonmail.ch>
2014-12-04 23:41:48 -0500

    I updated ethan's code, but this returns no keys...

--- failedxyz-230/TFCipher.py
+++ failedxyz-230/TFCipher.py
@@ -13,7 +13,8 @@ def whale(key , cipher):
 		tuna = Twofish(key)
 		print(tuna.decrypt(cipher).decode())
 	except:
-		print("Key :" + key + " failed")
+		#print("Key :" + key + " failed")
+		pass; # save your eye some trouble
 
 def brute():
 	alphabet = "a b c d e f g h j k l m n o p q r s t u v w x y z "
@@ -22,10 +23,21 @@ def brute():
 	L = alphabet.split()
 	L.append(" ")
 	tmp = ""
-	for i in range(0 , len(L)):
-		for subset in combinations(L , i):
+	for i in (2,3,4):
+		print i;
+		for subset in combinations(L , 2):
 			for subsubset in permutations(subset):
 				tmp = "".join(subsubset)
-				whale(tmp , "5ktxaA0e8yaL5tvrXjfKjM4ZYGmgVtSvsS7yZoH9udI=")
+				testVal = tmp * (8/i)
+				if i == 3:
+					testVal = testVal+tmp
+				while len(testVal) <= 32:
+					whale(testVal , "5ktxaA0e8yaL5tvrXjfKjM4ZYGmgVtSvsS7yZoH9udI=")
+					testVal = testVal + tmp
 
 brute()
+#osu = "osu"
+#key = "osu";
+#while len(key) <= 32:
+#	whale(key,"5ktxaA0e8yaL5tvrXjfKjM4ZYGmgVtSvsS7yZoH9udI=")
+#	key = key + osu;
